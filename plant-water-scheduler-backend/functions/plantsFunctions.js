3b0994b3651e12142078a9c9daa4ca9087a15a08
Gergely Varga <varga.gergely098@gmail.com>
2020-12-29 23:00:01 +0100

    plantsFunctions basics created

--- plant-water-scheduler-backend/functions/plantsFunctions.js
+++ plant-water-scheduler-backend/functions/plantsFunctions.js
@@ -11,13 +11,67 @@ class PlantsFunctions {
    * @param {Object} params
    * @return {Promise<any>}
    */
-  createPlant(params) {
+  async createPlant(params) {
     if (!params || Array.isArray(params) || typeof params !== 'object')
       throw new Error('Invalid parameter object in createPlant')
     await db.transaction(async (t) => {
       await Plants.create(params, { transaction: t })
     })
   }
+
+  async getPlantById(plantId) {
+    if (!plantId)
+      throw new Error('Missing or incorrect parameter plantId | PlantFunctions.getPlantById')
+    const result = await Plants.findOne({ 
+      where: {
+        id: plantId
+      }
+     })
+  }
+
+  async getPlantsByGroup(groupId) {
+    if (!groupId)
+    throw new Error('Missing or invalid groupId | PlantFunctions.getPlantsByGroup')
+    const result = await Plants.findAll({
+      where: {
+        id: groupId
+      }
+    })
+    return result
+  }
+
+  async getAllPlants() {
+    const result = await Plants.findAll()
+    return result
+  }
+   /**
+   * @description Updates a plant where id matches
+   * @param {Number} id 
+   * @param {Object} params
+   * @return {Promise<any>}
+   */
+  async updatePlant(plantId, params) {
+    if (!plantId || typeof params !== 'object' || Array.isArray(params))
+      throw new Error('Missing or incorrect parameter plantId or params @param {Object}')
+    await db.transaction(async (t) => {
+      Plants.update(params, {
+        transaction: t, where: {id: plantId}
+      })
+    })
+  }
+
+  async deletePlant(plantId) {
+    if (!plantId)
+      throw new Error('Missing or invalid param plantId | PlantFunctions.deletePlant')
+    await db.transaction(async (t) => {
+      await Plants.destroy({
+        where: {
+          id: plantId
+        }
+      })
+    })
+  }
+
 }
 
 module.exports = PlantsFunctions
