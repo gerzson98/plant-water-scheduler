3888e5f1dcfca565d378222e0678186558368a08
Gergely Varga <varga.gergely098@gmail.com>
2020-12-30 01:00:19 +0100

    function classes imported to functionService

--- plant-water-scheduler-backend/services/functionService.js
+++ plant-water-scheduler-backend/services/functionService.js
@@ -5,8 +5,19 @@ const GroupFunctions = require("../functions/groupFunctions")
 
 /** @typedef GroupFunctions*/
 const GroupService = new GroupFunctions()
+/** @typedef HomeFunctions*/
 const HomeService = new HomeFunctions()
+/** @typedef PlantFunctions*/
+const PlantService = new PlantFunctions()
+/** @typedef SpeciesFunctions*/
+const SpeciesService = new SpeciesFunctions()
+/** @typedef UserFunctions*/
+const UserService = new UserFunctions()
 
 module.exports = {
-  GroupService
+  GroupService,
+  HomeService,
+  PlantService,
+  SpeciesService,
+  UserService
 }
